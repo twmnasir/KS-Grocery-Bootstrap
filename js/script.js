5c3980015425ba8efa44ce534e640da52b17ae00
twmnasir <gnnasir123@gmail.com>
2024-08-21 20:39:31 +0600

    n/n

--- js/script.js
+++ js/script.js
@@ -23,5 +23,59 @@
  // pre loader
  window.addEventListener('load', function () {
     document.body.classList.add('loaded');
+    document.getElementById('search_list').style.display = "none";
 });
 // preloader
+
+//  Search ul li list
+document.getElementById('search_list').style.display = "none";
+document.getElementById('search_input').addEventListener('input', function(event) {
+    document.getElementById('search_list').style.display = "block";
+});
+document.getElementById('search_input').addEventListener('input', function(event) {
+    let search_input_value = event.target.value;
+    let search_list = document.getElementById('search_list');
+
+    if (search_input_value.trim() !== "") {
+        search_list.style.display = 'block';
+    } else {
+        search_list.style.display = 'none';
+    }
+});
+
+//  Search ul li list
+
+
+
+
+ //======== tiny slider for testimonial-one
+//  document.querySelector('tns-liveregion').textContent = '';
+//  document.querySelector('current').textContent = '';
+ tns({
+    autoplay: true,
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    gutter: 0,
+    container: ".testimonial-one-active",
+    center: true,
+    nav: true,
+    controls: false,
+    speed: 200,
+    controlsText: [
+    //   '<i class="lni lni-arrow-left-circle"></i>',
+    //   '<i class="lni lni-arrow-right-circle"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+
+      992: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+ //======== tiny slider for testimonial-one
